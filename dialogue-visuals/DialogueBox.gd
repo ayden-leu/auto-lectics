e35a9268321d9594fb94f2cb26da3c0c8504c536
ayden-leu <ajleu@ucdavis.edu>
2026-01-28 23:54:03 -0800

    options can be spawned.  functionality to pick an option and continue dialogue tree.  ability to delay option spawning.

--- dialogue-visuals/DialogueBox.gd
+++ dialogue-visuals/DialogueBox.gd
@@ -1,36 +1,53 @@
 extends Node3D
 class_name DialogueBox
 
+signal update_me
+
 @onready var myLabel:Label3D = $DialogueLabel
-#var optionScenePath:String = "res://dialogue-visuals/dialogue-option.tscn"
+var text:String = "":
+	set(value):
+		text = value
+		myLabel.text = value
 
-var delayStartShowingOptions:float = 1.0
-var delayBetweenOptions:float = 0.5
-
-var optionData:Array = []
+@onready var optionScene:Resource = preload("res://dialogue-visuals/DialogueOption.tscn")
 @onready var optionSpawnPositions = $OptionPositions.get_children()
+var optionData:Array = []
+var loadedOptions:Array = []
 
 func _ready() -> void:
-	visible = false
+	#visible = false
+	pass
 	
 func _process(_delta: float) -> void:
 	pass
 
-
-func initialize() -> void:
-	visible = true
-	
-	await get_tree().create_timer(delayStartShowingOptions).timeout
-	
-	for option in optionData:
-		#spawnOption()
-		pass
-
-func setDialogueText(text:String) -> void:
-	myLabel.text = text
-
 func loadOptionData(options:Array) -> void:
 	optionData = options
 
-func spawnOption(optionData:Dictionary, position:Vector3) -> void:
-	pass
+func createOptions() -> void:
+	for i in range(optionData.size()):
+		spawnOption(optionData[i], optionSpawnPositions[i])
+
+func spawnOption(data:Dictionary, marker:Marker3D) -> void:
+	var option:DialogueOption = optionScene.instantiate()
+	marker.add_child(option)
+	loadedOptions.push_back(option)
+	
+	option.text = data.text
+	option.nextDialogue = data.nextID
+	option.spawnDelay = data.spawnDelay
+	option.connect("option_picked", _onOptionPicked)
+	option.spawn()
+	
+func _onOptionPicked(data) -> void:
+	for _i in range(loadedOptions.size()):
+		var toKill:DialogueOption = loadedOptions.pop_front()
+		toKill.kill()
+		
+	emit_signal("update_me", data)
+
+func dummyFunction(index:int) -> void:
+	loadedOptions[index-1].picked()
+
+func kill() -> void:
+	queue_free()
--- dialogue-visuals/NPC.gd
+++ dialogue-visuals/NPC.gd
@@ -1,47 +1,102 @@
 extends Node3D
 
 @onready var dialogueBoxScene:Resource = preload("res://dialogue-visuals/DialogueBox.tscn")
+@onready var dialogueBoxAnchor:Marker3D = $DialogueBoxAnchor
 var dialogueBox:DialogueBox = null
 
-var dialogue:Dictionary = {
-	"initial": "who are you.  why are you interacting with me...",
-	"options": [
-		{
-			"text": "your shirt looks cool",
-			"type": "good"
-		},
-		{
-			"text": "where are you library?",
-			"type": "bad"
-		},
-		{
-			"text": "oh sorry, i thought you were someone else",
-			"type": "neutral"
-		}
-	]
-}
+var idleDialogueIndex:int = 0
 
+var dialogue:Array[Dictionary] = [
+	{
+		"id": 0,
+		"initial": "who are you.  why are you interacting with me...",
+		"options": [
+			{
+				"text": "1 your shirt looks cool",
+				"type": "good",
+				"spawnDelay": 3.0,
+				"nextID": 1,
+			},
+			{
+				"text": "2 where are you library?",
+				"type": "bad",
+				"spawnDelay": 0.5,
+				"nextID": 2
+			},
+			{
+				"text": "3 oh sorry, i thought you were someone else",
+				"type": "neutral",
+				"spawnDelay": 1.0,
+				"nextID": 3
+			}
+		]
+	},
+	{
+		"id": 1,
+		"initial": "oh thanks",
+		"options": []
+	},
+	{
+		"id": 2,
+		"initial": "what? weirdo",
+		"options": []
+	},
+	{
+		"id": 3,
+		"initial": "ah no problem",
+		"options": []
+	}
+]
+
+var delayStartShowingOptions:float = 1.0
+
+var dummyCounter:int = 0
 
 func _ready() -> void:
-	spawnDialogue()
+	pass
 
 func _process(_delta: float) -> void:
 	pass
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		loadData(dialogue)
-		dialogueBox.initialize()
+	if Input.is_action_just_pressed("ui_accept") and dummyCounter == 0:
+		_onInteraction()
+		dummyCounter = 1
+	
+	if dummyCounter == 1:
+		if Input.is_key_pressed(KEY_1):
+			dialogueBox.dummyFunction(1)
+			dummyCounter = 0
+		elif Input.is_key_pressed(KEY_2):
+			dialogueBox.dummyFunction(2)
+			dummyCounter = 0
+		elif Input.is_key_pressed(KEY_3):
+			dialogueBox.dummyFunction(3)
+			dummyCounter = 0
 
 func spawnDialogue() -> void:
 	if dialogueBox != null:
 		return
 	
 	dialogueBox = dialogueBoxScene.instantiate()
-	add_child(dialogueBox)
-	dialogueBox.position = Vector3(0, 1.214, 1.941)
-	dialogueBox.rotation_degrees = Vector3(0.0, 16.8, 0.0)
+	dialogueBox.connect("update_me", loadNextDialogue)
+	dialogueBoxAnchor.add_child(dialogueBox)
 	
 func loadData(dialogueEntry:Dictionary) -> void:
-	dialogueBox.setDialogueText(dialogueEntry.initial)
-	dialogueBox.loadOptionData(dialogueEntry.options)
+	dialogueBox.text = dialogueEntry.initial
+	dialogueBox.optionData = dialogueEntry.options
+
+func loadNextDialogue(data) -> void:
+	loadData(dialogue[data])
+	
+	await get_tree().create_timer(delayStartShowingOptions).timeout
+	
+	if dialogue[data].options.size() == 0:
+		dialogueBox.kill()
+		return
+
+	dialogueBox.createOptions()
+
+func _onInteraction() -> void:
+	spawnDialogue()
+	loadNextDialogue(idleDialogueIndex)
