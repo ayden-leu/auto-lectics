96ab0a529b33fcfd9a8cc4d4d2f8190a66872dc4
ayden-leu <ajleu@ucdavis.edu>
2026-02-05 18:28:36 -0800

    renamed some stuff.  implementation looks fine,

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,76 +2,76 @@
 extends CharacterBody3D
 class_name Player
 
-
-
 @onready var cameraAnchor:Marker3D = %CameraAnchor
 @onready var interactionRaycast:RayCast3D = %InteractionRaycast
 
-# -----------------------------
-# Movement Tuning (Heavy Feel)
-# -----------------------------
+# TODO:  determine better values for min, max, and step once we figure out the real player size.
+# =======================
 @export_group("Movement - Ground")
-@export_range(0.0, 30.0, 0.1) var max_speed: float = 5.0
-@export_range(0.0, 80.0, 0.5) var acceleration: float = 18.0   # smaller means more time to acc
-@export_range(0.0, 120.0, 0.5) var deceleration: float = 28.0   # smaller _ more hard to stop
-@export_range(0.0, 20.0, 0.1) var turn_deceleration_boost: float = 6.0 # setting for turn
+## The player's maximum speed.
+@export_range(0.0, 30.0, 0.1) var maxSpeed: float = 5.0
+## The player's acceleration.
+## The smaller the value, the more time it takes to reach max speed.
+@export_range(0.0, 80.0, 0.5) var acceleration: float = 18.0
+## THe player's deceleration.
+## The smaller the value, the more time it takes to stop moving.
+@export_range(0.0, 120.0, 0.5) var deceleration: float = 28.0
+## Gives you a boost in movement speed when turning.
+@export_range(0.0, 20.0, 0.1) var turnDecelerationBoost: float = 6.0 # setting for turn
 
 @export_group("Movement - Air")
-@export_range(0.0, 60.0, 0.5) var air_acceleration: float = 10.0
-@export_range(0.0, 60.0, 0.5) var air_deceleration: float = 6.0
-@export_range(0.0, 1.0, 0.01) var air_control: float = 0.45
+## The player's air acceleration.
+## The smaller the value, the more time it takes to reach max speed.
+@export_range(0.0, 60.0, 0.5) var airAcceleration: float = 10.0
+## The player's air deceleration.
+## The smaller the value, the more time it takes to stop moving.
+@export_range(0.0, 60.0, 0.5) var airDeceleration: float = 6.0
+## The smaller the value, the more time it takes to accelerate and decelerate.
+@export_range(0.0, 1.0, 0.01) var airControl: float = 0.45
 
-# -----------------------------
-# Jump Tuning
-# -----------------------------
 @export_group("Jump")
-@export_range(0.1, 10.0, 0.05) var jump_height: float = 1.2
-@export_range(0.05, 2.0, 0.01) var time_to_apex: float = 0.4
-# “到达最高点的时间”越长越飘
+## The player's jump height.
+@export_range(0.1, 10.0, 0.05) var jumpHeight: float = 1.2
+## The amount of time it takes to reach the peak of the jump.
+@export_range(0.05, 2.0, 0.01) var timeToApex: float = 0.4
+## The amount of time the player stays at the peak of the jump.
+@export_range(0.0, 0.35, 0.01) var apexHangTime: float = 0.04
+## The higher the value, the faster the player falls.
+@export_range(1.0, 4.0, 0.05) var fallGravityMultiplier: float = 2.0
 
-@export_range(0.0, 0.35, 0.01) var apex_hang_time: float = 0.04
 
+var _jumpVelocity: float = 0.0
+var _gravityUp: float = 0.0
+var _gravityDown: float = 0.0
 
-@export_range(1.0, 4.0, 0.05) var fall_gravity_multiplier: float = 2.0
-@export_range(0.6, 2.0, 0.05) var low_jump_multiplier: float = 1.35
-
-
-# -----------------------------
-# Internals
-# -----------------------------
-var _jump_velocity: float = 0.0
-var _gravity_up: float = 0.0
-var _gravity_down: float = 0.0
-
-var _was_on_floor: bool = false
-var _time_since_apex: float = 0.0
-var _apex_hang_active: bool = false
+var _wasOnFloor: bool = false
+var _timeSinceApex: float = 0.0
+var _apexHangActive: bool = false
 
 
 func _ready() -> void:
 	_recompute_jump_params()
-	
 
 func _process(_delta: float) -> void:
 	# Dev-ing stuff
 	if Engine.is_editor_hint():
 		update_configuration_warnings()
 		
-func _notification(what: int) -> void:
+func _notification(_what: int) -> void:
 	#unfinished wait for connect to a notficiation function that able to change data after it had been edited in  editor
 	#if what == NOTIFICATION_EDITOR_PROPERTY_CHANGED:
 		_recompute_jump_params()
 
 
 func _recompute_jump_params() -> void:
-	#avioding 0 set make system bug
+	# avioding 0 set make system bug
 
-	#finding g from the given h and t for reaching max hight
-	_gravity_up = (2.0 * jump_height) / (time_to_apex * time_to_apex)
-	_jump_velocity = _gravity_up * time_to_apex
+	# finding g from the given h and t for reaching max hight
+	_gravityUp = (2.0 * jumpHeight) / (timeToApex * timeToApex)
+	_jumpVelocity = _gravityUp * timeToApex
 
 	# able to fall down faster
-	_gravity_down = _gravity_up * fall_gravity_multiplier
+	_gravityDown = _gravityUp * fallGravityMultiplier
 	
 
 func _physics_process(delta: float) -> void:
@@ -83,63 +83,45 @@ func _physics_process(delta: float) -> void:
 
 
 func _apply_vertical_physics(delta: float) -> void:
-	var on_floor := is_on_floor()
-
-	if not on_floor:
+	if not is_on_floor():
 		#if jumping, enable the hang
 		if velocity.y > 0.0:
-			_time_since_apex = 0.0
-			_apex_hang_active = false
+			_timeSinceApex = 0.0
+			_apexHangActive = false
 			# when reaching max hight, slower bit fall down speed to make jump more smoth.
-		elif velocity.y <= 0.0 and not _apex_hang_active and apex_hang_time > 0.0:
-			_apex_hang_active = true
-			_time_since_apex = 0.0
+		elif velocity.y <= 0.0 and not _apexHangActive and apexHangTime > 0.0:
+			_apexHangActive = true
+			_timeSinceApex = 0.0
 
-		#
-		if _apex_hang_active:
-			_time_since_apex += delta
-			if _time_since_apex < apex_hang_time:
-				velocity.y -= _gravity_up * 0.15 * delta
+		if _apexHangActive:
+			_timeSinceApex += delta
+			if _timeSinceApex < apexHangTime:
+				velocity.y -= _gravityUp * 0.15 * delta
 				return
 			else:
-				_apex_hang_active = false
+				_apexHangActive = false
 
-		
 		if velocity.y > 0.0:
-			velocity.y -= _gravity_up * delta
+			velocity.y -= _gravityUp * delta
 		else:
-			velocity.y -= _gravity_down * delta
+			velocity.y -= _gravityDown * delta
 	else:
-		_apex_hang_active = false
-		_time_since_apex = 0.0
+		_apexHangActive = false
+		_timeSinceApex = 0.0
 
-	_was_on_floor = on_floor
+	_wasOnFloor = is_on_floor()
 
 func jump() -> void:
 	# jump enable need to set
 	_recompute_jump_params()
-	velocity.y = _jump_velocity
-	_apex_hang_active = false
-	_time_since_apex = 0.0
+	velocity.y = _jumpVelocity
+	_apexHangActive = false
+	_timeSinceApex = 0.0
 
-
-## Modifies velocity so `move_and_slide()` can move the player
-#func handleDirectionInput(direction:Vector3) -> void:
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-		
 func handleDirectionInput(direction: Vector3) -> void:
-	# 只处理水平速度
 	var target := Vector3.ZERO
 	if direction != Vector3.ZERO:
-		target = direction.normalized() * max_speed
-
-	var on_floor := is_on_floor()
-
+		target = direction.normalized() * maxSpeed
 	
 	var current_h := Vector3(velocity.x, 0.0, velocity.z)
 	var desired_h := Vector3(target.x, 0.0, target.z)
@@ -149,20 +131,20 @@ func handleDirectionInput(direction: Vector3) -> void:
 	var accel := acceleration
 	var decel := deceleration
 
-	if not on_floor:
-		accel = air_acceleration
-		decel = air_deceleration
+	if not is_on_floor():
+		accel = airAcceleration
+		decel = airDeceleration
+		
+	var physicsDelta:float = get_physics_process_delta_time()
 
-	# 用 move_toward 做“渐进式变化”
 	if desired_h.length() > 0.0:
-		var step := accel * (1.0 if on_floor else air_control) * get_physics_process_delta_time()
-		if turning and on_floor:
-			# 转向时先“刹一下”，避免飘移感太强
-			var turn_step := (decel + turn_deceleration_boost) * get_physics_process_delta_time()
+		var step:float = accel * (1.0 if is_on_floor() else airControl) * physicsDelta
+		if turning and is_on_floor():
+			var turn_step:float = (decel + turnDecelerationBoost) * physicsDelta
 			current_h = current_h.move_toward(Vector3.ZERO, turn_step)
 		current_h = current_h.move_toward(desired_h, step)
 	else:
-		var step := decel * get_physics_process_delta_time()
+		var step:float = decel * physicsDelta
 		current_h = current_h.move_toward(Vector3.ZERO, step)
 
 	velocity.x = current_h.x
@@ -173,7 +155,6 @@ func _onMouseMoved(distanceMoved:Vector2) -> void:
 	#print(name + ": mouse moved")
 	rotation_degrees.y += -distanceMoved.x
 	interactionRaycast.rotation_degrees.x -= -distanceMoved.y
-
 
 func _onInteractPressed() -> void:
 	#print(name + ": interact pressed")
