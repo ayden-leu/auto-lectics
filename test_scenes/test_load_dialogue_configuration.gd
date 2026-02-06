455935f64650bf9d43ff77c3a23568cc51f108a9
ayden-leu <ajleu@ucdavis.edu>
2026-02-05 18:39:04 -0800

    tweaked test scene

--- test_scenes/test_load_dialogue_configuration.gd
+++ test_scenes/test_load_dialogue_configuration.gd
@@ -2,25 +2,34 @@ extends Node3D
 
 # TODO:  test the functions implemented in dialogue_db.gd
 
-@export var dialogue_json_path := "res://dialogue_objects/NPC_Test/Dialogue1a.json"
+@export var dialogueID:String = "Dialogue1a"
+var dialogueStorageLocation:String = "res://dialogue_objects/NPC_Test/"
+var dialogueFileType:String = ".json"
 
 func _ready() -> void:
 	var loader := DialogueLoader.new()
-	var dlg := loader.load_dialogue_node_file(dialogue_json_path)
+	var dialogue := loader.load_dialogue_node_file(
+		dialogueStorageLocation + dialogueID + dialogueFileType
+	)
 
 	print("--- DIALOGUE ---")
-	print("text:", dlg.get("text"))
-	print("type:", dlg.get("type"))
-	print("font:", dlg.get("font"))
-	print("writeSpeed:", dlg.get("writeSpeed"), " custom:", dlg.get("writeSpeedCustom"))
-	print("resolved cps:", loader.resolve_write_speed_chars_per_sec(dlg))
-	print("sfx:", dlg.get("sfx"))
-	print("bg theme:", dlg.get("backgroundTheme"))
-	print("particles:", dlg.get("particles"))
-	print("options:", dlg.get("options", []).size())
-
-	if dlg.get("options", []).size() > 0:
-		print("--- OPTION[0] ---")
-		var o0: Dictionary = dlg["options"][0]
-		print(o0)
-		print("resolved cps:", loader.resolve_write_speed_chars_per_sec(o0))
+	print("text: ", dialogue.get("text"))
+	print("type: ", dialogue.get("type"))
+	print("font: ", dialogue.get("font"))
+	print("writeSpeed: ", dialogue.get("writeSpeed"))
+	print("writeSpeedCustom: ", dialogue.get("writeSpeedCustom"))
+	print("resolved cps: ", loader.resolve_write_speed_chars_per_sec(dialogue))
+	print("sfx: ", dialogue.get("sfx"))
+	print("bg theme: ", dialogue.get("backgroundTheme"))
+	print("particles: ", dialogue.get("particles"))
+	print("options: ", dialogue.get("options", []).size())
+	print("")
+	
+	var counter:int = 0
+	for option in dialogue.get("options", []):
+		print("--- OPTION [", counter, "] ---")
+		print(option)
+		print("resolved cps:", loader.resolve_write_speed_chars_per_sec(option))
+		print("")
+		
+		counter += 1
