53080635082d59f7f70243bab2a17bd2d704091b
ayden-leu <ajleu@ucdavis.edu>
2026-02-05 19:03:12 -0800

    cleaned some npc code

--- entities/npcs/npc.gd
+++ entities/npcs/npc.gd
@@ -25,7 +25,8 @@ func spawnDialogue() -> void:
 		return
 
 	dialogueBox = dialogueBoxScene.instantiate()
-
+	
+	# TODO:  verify this note
 	# IMPORTANT CHANGE:
 	# DialogueBox should emit "update_me" with the option's nextID (String),
 	# not an array index. If it currently emits an int, update DialogueBox (see below).
@@ -33,52 +34,27 @@ func spawnDialogue() -> void:
 
 	dialogueBoxAnchor.add_child(dialogueBox)
 
-# Adapter: convert loaded JSON dict into what your DialogueBox expects
-func loadData(dlg: Dictionary) -> void:
-	# Your DialogueBox expects:
-	# dialogueBox.text = dialogueEntry.initial
-	# dialogueBox.optionData = dialogueEntry.options
-	#
-	# Our loader returns:
-	# dlg["text"] and dlg["options"] (options include "text", "spawnDelay", "nextID", etc.)
+func loadData(dialogueData: Dictionary) -> void:
+	dialogueBox.text = dialogueData.text
+	dialogueBox.optionData = dialogueData.options
 
-	dialogueBox.text = dlg.get("text", "")
-
-	# If your DialogueBox expects option keys exactly: text/type/spawnDelay/nextID, we're good.
-	# If it expects different keys, remap here.
-	dialogueBox.optionData = dlg.get("options", [])
-
-func loadNextDialogue(next_id: Variant) -> void:
-	# next_id should be a String ("" means end).
-	# But depending on your signal, it could come in as Variant; handle safely.
-	var id_str := ""
-	if typeof(next_id) == TYPE_STRING:
-		id_str = next_id
-	elif typeof(next_id) == TYPE_INT:
-		# If DialogueBox still sends ints, this is legacy behaviour; end safely.
-		push_warning("NPC: DialogueBox sent int next_id. Update DialogueBox to send nextID String.")
-		id_str = ""
-	else:
-		id_str = str(next_id)
-
-	if id_str == "":
+func loadNextDialogue(nextDialogueID: String) -> void:
+	# TODO:  maybe remove this part
+	if nextDialogueID == "":
 		_end_dialogue()
 		return
 
-	currentDialogueID = id_str
-
-	var dlg := Globals.loadDialogueNode(myName, currentDialogueID)
+	currentDialogueID = nextDialogueID
+	var dialogue := Globals.loadDialogueNode(myName, currentDialogueID)
 	# this never ran
 	#if dlg.is_empty():
 		#_end_dialogue()
 		#return
-
-	loadData(dlg)
+	loadData(dialogue)
 
 	await get_tree().create_timer(delayStartShowingOptions).timeout
-
-	var options: Array = dlg.get("options", [])
-	if options.size() == 0:
+ 
+	if dialogue.options.size() == 0:
 		_end_dialogue()
 		return
 
