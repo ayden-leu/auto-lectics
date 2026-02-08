d413ed5f9360216174bf03ca4ac1efd3e92cde41
ayden-leu <ajleu@ucdavis.edu>
2026-02-08 03:02:17 -0800

    player_camera.gd:  Documentation

--- entities/player/player_camera.gd
+++ entities/player/player_camera.gd
@@ -2,14 +2,18 @@
 extends Node3D
 class_name PlayerCamera
 
+## The target the camera wants to look at.
 @export var focus:Node3D:
 	set(newFocus):
 		focus = newFocus
 		update_configuration_warnings()
 
+## The actual camera.
 @onready var camera:Camera3D = $Camera3D
 
+## Whether the camera is in first person mode or not.
 var actAsFocus:bool = true
+## How far away from the focus the camera should be when in third person mode.
 var distanceFromOrigin:Vector3 = Vector3(0, 5, 10)
 
 func _ready() -> void:
@@ -29,15 +33,17 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug_2"):
 		actAsFocus = !actAsFocus
 
+## Handles logic for first person mode.
 func firstPersonMode() -> void:
 	camera.position = Vector3.ZERO
 	camera.rotation_degrees = Vector3.ZERO
 
+## Handles logic for third person mode.
 func thirdPersonMode() -> void:
 	camera.position = distanceFromOrigin
 	camera.look_at(global_position)
 
-# Rotates the camera horizontally and vertically when the mouse moves
+## Rotates the camera horizontally and vertically when the mouse moves
 func _onMouseMoved(distanceMoved:Vector2) -> void:
 	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 		return
@@ -54,7 +60,7 @@ func _get_configuration_warnings() -> PackedStringArray:
 	
 	if not focus:
 		warnings.push_back(
-			"No object has been assigned to the Focus property,
+			"No object has been assigned to the Focus property.
 		")
 	
 	return warnings
