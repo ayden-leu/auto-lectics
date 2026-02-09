d0e65cf0e46e2a496b491d3947f4539e2a2b38be
ayden-leu <ajleu@ucdavis.edu>
2026-02-09 05:04:19 -0800

    fixed bug with HUD crosshair

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -3,6 +3,4 @@ extends Control
 # TODO:  actually code this because this is just quick and dirty
 
 func _ready() -> void:
-	var windowSize := DisplayServer.window_get_size()
-	size = windowSize
-	$Container.size = windowSize
+	pass
