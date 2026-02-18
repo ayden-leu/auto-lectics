f586bbc9d3f1f9b0592ff98acc4e3adb9597a88a
ayden-leu <ajleu@ucdavis.edu>
2026-02-18 02:46:09 -0800

    converted into a node that you add to scenes
    the previous implementation made it always loaded.
    this would be a problem as it assumes the loaded scene is always a world.

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -1,6 +1,9 @@
 extends Control
 
 # TODO:  actually code this because this is just quick and dirty
+# TODO:  figure out where to actually place the HUD node.
+#			currently, it's part of the PlayerCamera scene, but this makes it inaccessible to other nodes (unless you use workarounds)
+#			should probably be part of the theorhetial "Scene Manager" node that we'll have to implement once we add menus.
 
 func _ready() -> void:
 	pass
