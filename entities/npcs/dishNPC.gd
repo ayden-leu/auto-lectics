2517fe12ca8bbc8c1242d30658dbf53a024b0a0c
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-03-06 16:04:47 -0800

    starting demo!!!

--- entities/npcs/dishNPC.gd
+++ entities/npcs/dishNPC.gd
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 		return
 	
 	time += delta;
-	rotation = Vector3(0, base_position.y +get_sine(), 0)
+	#rotation = Vector3(0, base_position.y +get_sine(), 0)
 	#position = Vector3(base_position.x, base_position.y + get_sine(), base_position.z)
 	#print(get_sine())
 	pass
