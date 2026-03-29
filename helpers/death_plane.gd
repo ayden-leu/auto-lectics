13c521f63576f703168a52890ff1293e0ad4ff9f
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-03-28 17:28:36 -0700

    added a cut to back on hitting death plane + slight delay on respawn

--- helpers/death_plane.gd
+++ helpers/death_plane.gd
@@ -8,6 +8,12 @@ func _ready() -> void:
 ## Causes player to respawn at last grounded location if they touch the death plane.
 func _on_body_entered(body: Node) -> void:
 	if body.has_method("respawn"):
+		## plays anmiation
+		$CanvasLayer/FadeRect/AnimationPlayer.play("cut_to_black")
+		
+		## waits a second before respawning
+		await get_tree().create_timer(1.0).timeout
 		body.respawn()
 	else:
 		push_warning("DeathPlane: Body missing respawn()")
+	
