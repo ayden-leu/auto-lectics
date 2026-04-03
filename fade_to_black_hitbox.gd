6de888003cd7ff266e5baecad131f25bd0cee749
MarcelleWeaver <marcelletweaver@gmail.com>
2026-04-02 18:28:03 -0700

    fade to black scene now also resets the game if your inactive
    this needs to be turned off for a non demo version

--- fade_to_black_hitbox.gd
+++ fade_to_black_hitbox.gd
@@ -4,8 +4,17 @@ extends Node3D
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	$CanvasLayer/FadeRect/AnimationPlayer.play("fade_to_black")
-	$CanvasLayer/Label/AnimationPlayer.play("text_fade_in")
+	$CanvasLayer/Label2/AnimationPlayer.play("text_fade_in")
 	$Timer.start()
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://entities/player/starting_screen_handler.tscn")
+
+func _on_inactivity_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://entities/player/starting_screen_handler.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event: 
+		$"Inactivity Timer".start(180)
+		print("restarting game due to inactivity")
+		
