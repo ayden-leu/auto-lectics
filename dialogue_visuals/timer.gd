4d1cc300be817a2982f2aafb3b064cdc1a5681c5
ayden-leu <ajleu@ucdavis.edu>
2026-04-08 15:06:22 -0700

    passthrough (TimerBar)

--- dialogue_visuals/timer.gd
+++ dialogue_visuals/timer.gd
@@ -1,10 +1,26 @@
 @tool
 extends Node3D
 class_name TimerBar
+## A 3D representation of a timer.  Left is positive Z, right is negative Z.
+## Make sure the mesh resource's "Local to Scene" property is enabled.
 
-## Emitted when the timer runs out.
-signal timeout
+# ------------------------------------------------
+# signals
+# ------------------------------------------------
+## Emitted when the time elapsed since starting is greater than or equal to the [member duration]
+signal timeout()
 
+# ------------------------------------------------
+# enums
+# ------------------------------------------------
+
+# ------------------------------------------------
+# constants
+# ------------------------------------------------
+
+# ------------------------------------------------
+# export variables
+# ------------------------------------------------
 ## Set to true when you want to start the timer.
 @export var shouldCountdown:bool = false:
 	set(value):
@@ -15,19 +31,34 @@ signal timeout
 @export var duration:float = 1.0
 ## The progress of the timer so far.
 @export_range(0.0, 1.0) var progress:float = 1.0
+## The mesh that represents the timer bar.
+@export var bar:MeshInstance3D
 
-## Holds a reference to the timer's visual bar.
-@onready var bar:MeshInstance3D = $Bar
+# ------------------------------------------------
+# onready variables
+# ------------------------------------------------
 
-## The original size of the timer bar. Currently hardcoded.
-const originalBarSize:Vector3 = Vector3(0.14, 2.42, 2.075)
-
+# ------------------------------------------------
+# normal variables referenced outside of script
+# ------------------------------------------------
 ## The amount of time passed since starting the timer.
 var timeElapsed:float = 0.0:
 	set(value):
 		timeElapsed = value
 		progress = 1 - timeElapsed / duration
-		updateBar()
+		_updateBar()
+
+# ------------------------------------------------
+# normal variables only referenced in script
+# ------------------------------------------------
+## The original size of the timer bar. Currently hardcoded.
+var _originalBarSize:Vector3
+
+# ------------------------------------------------
+# functions like _ready, _process, and _physics_process
+# ------------------------------------------------
+func _init() -> void:
+	_originalBarSize = Vector3(0.14, 0.12, 2.075)
 
 func _ready() -> void:
 	# makes sure not to run code if in editor
@@ -35,15 +66,20 @@ func _ready() -> void:
 		return
 	else:
 		visible = false
+	
+	_originalBarSize = bar.mesh.size
 
 func _process(delta: float) -> void:
-	updateBar()
+	_updateBar()
 	
 	if shouldCountdown:
 		timeElapsed += delta
 		if timeElapsed >= duration:
-			onTimeout()
+			_timeout()
 
+# ------------------------------------------------
+# functions referenced outside of this script
+# ------------------------------------------------
 ## Starts the timer.
 func start(timeSec:float = -1.0) -> void:
 	if timeSec > 0.0:
@@ -64,8 +100,11 @@ func stop() -> void:
 	timeElapsed = 0
 	visible = false
 
+# ------------------------------------------------
+# functions only referenced inside this script
+# ------------------------------------------------
 ## Ran when the the time elapsed is beyond the duration.
-func onTimeout() -> void:
+func _timeout() -> void:
 	if not shouldCountdown:
 		return
 	
@@ -73,6 +112,14 @@ func onTimeout() -> void:
 	timeout.emit()
 
 ## Updates the size of the bar based on the timer's progress.
-func updateBar() -> void:
-	bar.mesh.size.z = originalBarSize.z * progress
-	bar.position.z = originalBarSize.z/2 * (1-progress)
+func _updateBar() -> void:
+	bar.mesh.size.z = _originalBarSize.z * progress
+	bar.position.z = _originalBarSize.z/2 * (1-progress)
+
+# ------------------------------------------------
+# functions that run when a signal is emitted
+# ------------------------------------------------
+
+# ------------------------------------------------
+# editor dev-ing functions like "_get_configuration_warnings()"
+# ------------------------------------------------
