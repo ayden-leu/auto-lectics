e91c7817801e66ae3f2d1034eaebfd5ecd694d21
ayden-leu <ajleu@ucdavis.edu>
2026-04-08 16:45:55 -0700

    passthrough (NPC and InteractableNPC)

--- entities/npcs/interactable_npc.gd
+++ entities/npcs/interactable_npc.gd
@@ -4,80 +4,148 @@ extends NPC
 class_name InteractableNPC
 ## The base class of all Interactable NPCs in the game.  Interactable NPCs allow the player to interact with them and initiate a dialogue event.
 
+# ------------------------------------------------
+# signals
+# ------------------------------------------------
 ## Emitted when a dialogue entry is fully displayed.
-signal dialogue_all_visible
+signal dialogue_all_visible()
 ## Emitted when a dialogue option is spawned.
-signal option_available
+signal option_available()
 ## Emitted when all dialogue options have been spawned.
-signal all_options_available
+signal all_options_available()
 ## Emitted when the dialogue tree reaches an end.
-signal finished_dialogue
+signal finished_dialogue()
 
-## Attach methods for the [DialogueBox]
-enum ATTACH_METHOD {
-	FOLLOW,  ## When spawned, the [DialogueBox] will be in the same relative position and rotation as this [InteractableNPC] all of the time.  If the [InteractableNPC] moves left, the DialogueBox will move left.  If the the [member dialogueBoxAnchor] is right above the [InteractableNPC] and the [InteractableNPC] flips upside down, the [DialogueBox] will be physically below the [InteractableNPC] and upside down.
-	STAY  ## When spawned, the [DialogueBox] will take the position and rotation of the [member dialogueBoxAnchor] at that moment and stay there.  If the [InteractableNPC] moves or rotates after this moment, the [DialogueBox] will stay in place.
+# ------------------------------------------------
+# enums
+# ------------------------------------------------
+## @deprecated
+## Attach methods for the [DialogueBox].
+enum AttachMethod {
+	FOLLOW,  ## When spawned, the [DialogueBox] will be in the same relative position and rotation as this [InteractableNPC] all of the time.  If the [InteractableNPC] moves left, the DialogueBox will move left.  If the the [member _dialogueBoxAnchor] is right above the [InteractableNPC] and the [InteractableNPC] flips upside down, the [DialogueBox] will be physically below the [InteractableNPC] and upside down.
+	STAY  ## When spawned, the [DialogueBox] will take the position and rotation of the [member _dialogueBoxAnchor] at that moment and stay there.  If the [InteractableNPC] moves or rotates after this moment, the [DialogueBox] will stay in place.
 }
 
-## The hitbox of this Interactable NPC.
-@export var hitbox:Area3D
-## Tells the game where to spawn a dialogue box when a player interacts with the Interactable NPC.
-@export var dialogueBoxAnchor:Marker3D
+# ------------------------------------------------
+# constants
+# ------------------------------------------------
+## @deprecated
+## [b]Internal-use only.[/b]  A reference to the [DialogueBox] scene.
+const _DIALOGUE_BOX_SCENE:Resource = preload(Globals.SCENES.DialogueBox)
+
+const _dialogueConsoleScene: Resource = preload(Globals.SCENES.DialogueConsoleUI)
+
+# ------------------------------------------------
+# export variables
+# ------------------------------------------------
+## The hitbox of this Interactable NPC.  Used to allow players to interact with
+## this [InteractableNPC].
+@export var _hitbox:Area3D
+## @deprecated
+## Tells the game where to spawn a [DialogueBox] when a player interacts with the Interactable NPC.
+@export var _dialogueBoxAnchor:Marker3D
+## @deprecated
 ## How the [DialogueBox] should act after beind spawned.
-@export var dialogueBoxAttachMethod:ATTACH_METHOD
+@export var _dialogueBoxAttachMethod:AttachMethod
+## If this Interactable NPC should only respond to interactions once.
+@export var _talkOnlyOnce:bool = true
 ## All dialogues belonging to this Interactable NPC will be under "dialogue_objects/[NPC name]"
 @export var initialDialogueID:String = ""
-## If this Interactable NPC should only respond to interactions once.
-@export var talkOnlyOnce:bool = true
 ## Whether this [InteractableNPC] looks at the player while the dialogue event is happening.
 @export var lookAtInteractorWhileTalking:bool = false
 
-const _dialogueBoxScene:Resource = preload(Globals.SCENES.DialogueBox)#
-const _dialogueConsoleScene: Resource = preload(Globals.SCENES.DialogueConsoleUI)
 
 
-## The hitbox's collision shape.  Gets set when the node is ready.
-var hitboxShapes:Array[CollisionShape3D]
-## Holds a reference to this Interactable NPC's dialogue box scene.
-var dialogueBox:DialogueBox = null
-var dialogueConsole: DialogueConsoleUI = null
-## Single-use boolean to determine if the dialogue box's signals have been connected to functions yet.
-var connectedDialogueBoxSignals:bool = false
-var connectedDialogueConsoleSignals: bool = false
+
+# ------------------------------------------------
+# onready variables
+# ------------------------------------------------
+
+# ------------------------------------------------
+# normal variables referenced outside of script
+# ------------------------------------------------
 ## Is true when their dialogue box is visible.
 var isTalking: bool = false
-## Keeps track of which dialogue object to reference at the moment.
-var currentDialogueID: String = ""
 ## Keeps track of if the player has interacted with this Interactable NPC.  If true, this NPC can no longer be talked to.
 var wasTalkedTo: bool = false
-## Tracks who is interacting with this NPC; curently can only be the player
-var currentInteractor:Node3D
-## Tracks whether the NPC should be patrolling
-var shouldPatrol:bool
+
+
 ## Array that stores previous dialogue history
 var dialogueHistory: Array[String] = []
 
+# ------------------------------------------------
+# normal variables only referenced in script
+# ------------------------------------------------
+## [b]Internal-use only.[/b]  The hitbox's collision shape.  Gets set when the node is ready.
+var _hitboxShapes:Array[CollisionShape3D]
+## @deprecated
+## [b]Internal-use only.[/b]  Holds a reference to this Interactable NPC's dialogue box scene.
+var _dialogueBox:DialogueBox = null
+## @deprecated
+## [b]Internal-use only.[/b]  Single-use boolean to determine if the dialogue box's signals have been connected to functions yet.
+var _connectedDialogueBoxSignals:bool = false
+## [b]Internal-use only.[/b]  Keeps track of which dialogue object to reference at the moment.
+var _currentDialogueID: String = ""
+## [b]Internal-use only.[/b]  Tracks who is interacting with this NPC; curently can only be the player
+var _currentInteractor:Node3D
+## [b]Internal-use only.[/b]  Tracks whether the NPC should be patrolling.
+## Mainly used to restore patrol state after finishing a dialogue interaction.
+var _shouldPatrol:bool
+
+
+var dialogueConsole: DialogueConsoleUI = null
+## Single-use boolean to determine if the dialogue console's signals have been connected to functions yet.
+var connectedDialogueConsoleSignals: bool = false
+
+# ------------------------------------------------
+# functions like _ready, _process, and _physics_process
+# ------------------------------------------------
 func _ready() -> void:
 	# Makes sure the code after this is only ran in-game
 	if Engine.is_editor_hint():
 		return
-	super()
-	shouldPatrol = patrolEnabled
 	
-	currentDialogueID = initialDialogueID
-	hitboxShapes = getHitboxShapes()
+	super()
+	_shouldPatrol = patrolEnabled
+	_currentDialogueID = initialDialogueID
+	_hitboxShapes = _getHitboxShapes()
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 		
 	super(delta)
-	if lookAtInteractorWhileTalking and currentInteractor and isTalking:
-		lookAtPosition = currentInteractor.global_position
+	if lookAtInteractorWhileTalking and _currentInteractor and isTalking:
+		lookAtPosition = _currentInteractor.global_position
 
-## Gets the hitbox's collision shapes.
-func getHitboxShapes() -> Array[CollisionShape3D]:
-	var children:Array[Node] = hitbox.get_children()
+# ------------------------------------------------
+# functions referenced outside of this script
+# ------------------------------------------------
+## Enables the Interactable NPC.  This will make it so the player can interact with this Interactable NPC.
+func enable() -> void:
+	_hitbox.monitorable = true
+	for shape in _hitboxShapes:
+		shape.set_deferred("disabled", false)
+	
+## Disables the Interactable NPC.  This will make it so the player cannot interact with this Interactable NPC.
+func disable() -> void:
+	_hitbox.monitorable = false
+	for shape in _hitboxShapes:
+		shape.set_deferred("disabled", true)
+
+## Resets the Interactable NPC to their default state.
+func reset() -> void:
+	if isTalking:
+		_endDialogue()
+	_currentDialogueID = initialDialogueID
+	wasTalkedTo = false
+
+# ------------------------------------------------
+# functions only referenced inside this script
+# ------------------------------------------------
+## [b]Internal-use only.[/b]  Gets the [member hitbox]'s collision shapes.
+func _getHitboxShapes() -> Array[CollisionShape3D]:
+	var children:Array[Node] = _hitbox.get_children()
 	var shapes:Array[CollisionShape3D] = []
 	
 	for child in children:
@@ -102,7 +170,7 @@ func connectDialogueConsoleSignals() -> void:
 		return
 	connectedDialogueConsoleSignals = true
 
-	dialogueConsole.option_chosen.connect(loadNextDialogue)
+	dialogueConsole.option_chosen.connect(loadNextDialogueConsole)
 	dialogueConsole.open_gate.connect(openGate)
 	dialogueConsole.request_back.connect(_on_console_request_back)
 	dialogueConsole.new_option_available.connect(_on_dialogue_box_new_option_spawned)
@@ -114,7 +182,7 @@ func disconnectDialogueConsoleSignals() -> void:
 		return
 	connectedDialogueConsoleSignals = false
 	
-	dialogueConsole.option_chosen.disconnect(loadNextDialogue)
+	dialogueConsole.option_chosen.disconnect(loadNextDialogueConsole)
 	dialogueConsole.open_gate.disconnect(openGate)
 	dialogueConsole.request_back.disconnect(_on_console_request_back)
 	dialogueConsole.new_option_available.disconnect(_on_dialogue_box_new_option_spawned)
@@ -136,26 +204,66 @@ func loadDialogueConsoleData(dialogueEntry: Dictionary) -> void:
 	dialogueConsole.show_dialogue_data(dialogueEntry)
 	dialogueConsole.loadOptionData(dialogueEntry.options)
 	dialogueConsole.prepare()
+## [b]Internal-use only.[/b]  Creates a [DialogueBox]. Only one can exist at a time.
+func _spawnDialogueBox() -> void:
+	if _dialogueBox != null:
+		return
+	
+	_dialogueBox = _DIALOGUE_BOX_SCENE.instantiate()
+	if(_dialogueBoxAttachMethod == AttachMethod.FOLLOW):
+		_dialogueBoxAnchor.add_child(_dialogueBox)
+		
+		# undo the scaling being inherited from this InteractableNPC
+		_dialogueBox.scale += Vector3.ONE - scale 
+	elif(_dialogueBoxAttachMethod == AttachMethod.STAY):
+		get_parent().add_child(_dialogueBox)
+		_dialogueBox.global_position = _dialogueBoxAnchor.global_position
+		_dialogueBox.global_rotation = _dialogueBoxAnchor.global_rotation
+
+## [b]Internal-use only.[/b]  Connects the [member dialogueBox] signals to functions.
+## Only needs to be ran once.
+func _connectDialogueBoxSignals() -> void:
+	if _connectedDialogueBoxSignals:
+		return
+	_connectedDialogueBoxSignals = true
+	
+	_dialogueBox.update_me.connect(_loadNextDialogue)
+	_dialogueBox.new_option_available.connect(_on_dialogue_box_new_option_spawned)
+	_dialogueBox.all_options_available.connect(_on_dialogue_box_all_options_available)
+	_dialogueBox.all_dialogue_text_visible.connect(_on_dialogue_box_all_dialogue_text_visible)
+
+## [b]Internal-use only.[/b]  Disconnects the [member dialogueBox] signals to functions.
+func _disconnectDialogueBoxSignals() -> void:
+	if not _connectedDialogueBoxSignals:
+		return
+	_connectedDialogueBoxSignals = false
+
+	_dialogueBox.update_me.disconnect(_loadNextDialogue)
+	_dialogueBox.new_option_available.disconnect(_on_dialogue_box_new_option_spawned)
+	_dialogueBox.all_options_available.disconnect(_on_dialogue_box_all_options_available)
+	_dialogueBox.all_dialogue_text_visible.disconnect(_on_dialogue_box_all_dialogue_text_visible)
 
 ## @deprecated
-## Loads the data of a dialogue object into the dialogue box. Make sure currentDialogueID is set to the dialogue you want to load before running.
-func loadDialogueBoxData(dialogueEntry:Dictionary) -> void:
-	dialogueBox.realOwner = self
-	dialogueBox.currentDialogueID = currentDialogueID
-	dialogueBox.mode = dialogueEntry.mode
-	if dialogueEntry.mode == "hectic":	
-		dialogueBox.hecticFailureDialogueID = dialogueEntry.nextOnHecticFailureID
-		dialogueBox.delayBtwnWriteDialogueAndOptions = 0.25  # arbitrary
-	dialogueBox.text = dialogueEntry.text
-	dialogueBox.textWriteSpeed = dialogueEntry.writeSpeedCustom
-	dialogueBox.loadSfx(dialogueEntry.sfx)
-	dialogueBox.loadOptionData(dialogueEntry.options)
-	dialogueBox.prepare()
+## [b]Internal-use only.[/b]  Loads the data of a dialogue object into the dialogue box.
+## Make sure [member _currentDialogueID] is set to the dialogue you want to load before running.
+func _loadDialogueData(dialogueEntry:Dictionary) -> void:
+	_dialogueBox.realOwner = self
+	_dialogueBox._currentDialogueID = _currentDialogueID
+	_dialogueBox.mode = dialogueEntry.mode
+	if dialogueEntry.mode == "hectic":
+		_dialogueBox.hecticFailureDialogueID = dialogueEntry.nextOnHecticFailureID
+		_dialogueBox.delayBtwnWriteDialogueAndOptions = 0.25  # arbitrary
+	_dialogueBox.text = dialogueEntry.text
+	_dialogueBox.textWriteSpeed = dialogueEntry.writeSpeedCustom
+	_dialogueBox.loadSfx(dialogueEntry.sfx)
+	_dialogueBox.loadOptionData(dialogueEntry.options)
+	_dialogueBox.prepare()
 
+	
 ## Loads the next dialogue to display.
-func loadNextDialogue(nextDialogueID: String, addToHistory: bool = true) -> void:
+func loadNextDialogueConsole(nextDialogueID: String, addToHistory: bool = true) -> void:
 	if nextDialogueID == "" and isTalking:
-		endDialogue()
+		_endDialogue()
 		return
 	
 	currentDialogueID = nextDialogueID
@@ -176,12 +284,30 @@ func _on_console_request_back() -> void:
 	dialogueHistory.pop_back()
 	dialogueConsole.add_player_text("back")
 	var previous_id: String = dialogueHistory.back()
-	loadNextDialogue(previous_id, false)
+	loadNextDialogueConsole(previous_id, false)
 	return
 
+## [b]Internal-use only.[/b]  Loads the next dialogue to display.
+func _loadNextDialogue(nextDialogueID: String) -> void:
+	#print("\nloading dialogue: [", nextDialogueID, "]")
+	if nextDialogueID == "" and isTalking:
+		_endDialogue()
+		return
+	
+	_currentDialogueID = nextDialogueID
+	
+	var dialogue:Dictionary = Globals.getDialogueNode(myName, _currentDialogueID)
+	#print("dialogue data: ", dialogue)
+	_loadDialogueData(dialogue)
+	_dialogueBox.start()
 
-## Ends the dialogue interaction.
-func endDialogue() -> void:
+## [b]Internal-use only.[/b]  Ends the dialogue interaction.
+func _endDialogue() -> void:
+	_disconnectDialogueBoxSignals()
+	if _dialogueBox != null:
+		_dialogueBox.kill()
+		_dialogueBox = null
+	
 	disconnectDialogueConsoleSignals()
 	if dialogueConsole != null:
 		dialogueConsole.kill()
@@ -193,40 +319,24 @@ func endDialogue() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 	isTalking = false
-	currentInteractor = null
-	if shouldPatrol:
+	_currentInteractor = null
+	if _shouldPatrol:
 		patrolEnabled = true
-	if talkOnlyOnce:
+	if _talkOnlyOnce:
 		wasTalkedTo = true
 
 	finished_dialogue.emit()
-
-## Enables the Interactable NPC.  This will make it so the player can interact with this Interactable NPC.
-func enable() -> void:
-	hitbox.monitorable = true
-	for shape in hitboxShapes:
-		shape.set_deferred("disabled", false)
-	
-## Disables the Interactable NPC.  This will make it so the player cannot interact with this Interactable NPC.
-func disable() -> void:
-	hitbox.monitorable = false
-	for shape in hitboxShapes:
-		shape.set_deferred("disabled", true)
-
-## Resets the Interactable NPC to their default state.
-func reset() -> void:
-	if isTalking:
-		endDialogue()
-	currentDialogueID = initialDialogueID
-	wasTalkedTo = false
-
-
+# ------------------------------------------------
+# functions that run when a signal is emitted
+# ------------------------------------------------
 ## Handles flow of what to do when this [InteractableNPC] is interacted with.
 func _on_interaction(interactor: Node3D = null) -> void:
 	if wasTalkedTo or isTalking:
 		return
 	
-	var currentRotation: Vector3 = rotation
+	# rotate to face player immediately to properly spawn dialogue box
+	# is a hacky work around to spawn the [DialogueBox] in the correct position.
+	var currentRotation:Vector3 = rotation
 	if lookAtInteractorWhileTalking and interactor:
 		look_at(Vector3(
 			interactor.global_position.x,
@@ -237,31 +347,34 @@ func _on_interaction(interactor: Node3D = null) -> void:
 	
 	isTalking = true
 	patrolEnabled = false
-	currentInteractor = interactor
-	dialogueHistory.clear()
+	_currentInteractor = interactor
 	
+	dialogueHistory.clear()
 	if currentInteractor and currentInteractor.has_method("set_input_frozen"):
 		currentInteractor.set_input_frozen(true)
 		Globals.inputHandler.lock_mouse_to_cursor()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+
+	#_spawnDialogueBox()
+	#_connectDialogueBoxSignals()
+	#_loadNextDialogue(initialDialogueID)
+
 	spawnDialogueConsole()
 	connectDialogueConsoleSignals()
-	loadNextDialogue(initialDialogueID)
+	loadNextDialogueConsole(initialDialogueID)
 	
+	# restore rotation from before hacky work around
 	rotation = currentRotation
 
-
-## Emits the "option_available" signal.
+## Emits [signal option_available].
 func _on_dialogue_box_new_option_spawned() -> void:
 	option_available.emit()
 
-
-## Emits the "all_options_available" signal.
+## Emits [all_options_available].
 func _on_dialogue_box_all_options_available() -> void:
 	all_options_available.emit()
 
-## Emits the "dialogue_all_visible" signal.
+## Emits [dialogue_all_visible].
 func _on_dialogue_box_all_dialogue_text_visible() -> void:
 	dialogue_all_visible.emit()
 
@@ -279,19 +392,22 @@ func openGate():
 	if $gateNode:
 		$gateNode.open_gate()
 
+# ------------------------------------------------
+# editor dev-ing functions like "_get_configuration_warnings()"
+# ------------------------------------------------
 # Dev-ing stuff
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings:Array[String] = []
 		
-	if not hitbox:
+	if not _hitbox:
 		warnings.push_back("This NPC doesn't have a hitbox assigned yet. This is needed to allow the player to interact with them. A hitbox is an Area3D node.")
-	elif hitbox.collision_layer != 4:
+	elif _hitbox.collision_layer != 4:
 		warnings.push_back("The hitbox's collision layer should only have square #3/Bit 2/the Interactable NPC layer enabled.")
 	
 	if initialDialogueID == "":
 		warnings.push_back("The initial dialogue ID is not set.")
 	
-	if not dialogueBoxAnchor:
+	if not _dialogueBoxAnchor:
 		warnings.push_back("A marker for the dialogue box has not been set yet.")
 	
 	var inheritedWarnings:PackedStringArray = super()
--- entities/npcs/npc.gd
+++ entities/npcs/npc.gd
@@ -2,48 +2,76 @@
 @tool
 extends Node3D
 class_name NPC
-## The base class of all NPCs in the game.  
+## The base class of all NPCs in the game.
 
+# ------------------------------------------------
+# signals
+# ------------------------------------------------
+
+# ------------------------------------------------
+# enums
+# ------------------------------------------------
 ## The ways an [NPC] can follow a [member patrolPath].
-enum PATH_FOLLOW_METHOD {
+enum PathFollowMethod {
 	LOOP,  ## When this [NPC] reaches the last point on the [member patrolPath], it will go to the starting point on the [member patrolPath] directly.
 	PING_PONG  ## When this [NPC] reaches the last point on the [member patrolPath], it will turn around and follow the [member patrolPath] in reverse.
 }
 
-## The main model of the NPC.  Not used for anything in the base NPC class, but may be used in classes or scripts that extend the NPC class.
+# ------------------------------------------------
+# constants
+# ------------------------------------------------
+
+# ------------------------------------------------
+# export variables
+# ------------------------------------------------
+## The main model.  Not used for anything in the base NPC class, but may be used in classes or scripts that extend the NPC class.
 @export var model:Node3D
 ## The name of the NPC.  Not used for anything in the base NPC class, but may be used in classes that extend the NPC class.
 @export var myName:String = ""
 
-## If this [NPC] should start patrolling.
+## If this [NPC] should be able to move along its [member patrolPath].
 @export var patrolEnabled: bool = false:
 	set(value):
 		patrolEnabled = value
 		notify_property_list_changed()
-
 @export_category("Patrolling")
 ## The path this [NPC] follows while patrolling.
 @export var patrolPath: Path3D
 ## How fast this [NPC] should move along the [member patrolPath]. 
 @export var patrolSpeed: float = 2.0
 ## How this [NPC] should patrol on its [member patrolPath].
-@export var pathFollowMode:PATH_FOLLOW_METHOD:
+@export var pathFollowMode:PathFollowMethod:
 	set(value):
 		pathFollowMode = value
 		notify_property_list_changed()
 ## If [member pathFollowMode] is set to [constant PING_PONG] and this [NPC] reaches the end of the path, it will wait this long in seconds before traversing the path backwards.
 @export var waitAtEndDuration: float = 0.0
-# unused, but could be helpful in the future
-#@export var face_move_direction: bool = true
+## Unused and doesn't do anything.
+@export var faceMoveDirection: bool = true
 
+# ------------------------------------------------
+# onready variables
+# ------------------------------------------------
+
+# ------------------------------------------------
+# normal variables referenced outside of script
+# ------------------------------------------------
+## If true, this [NPC] will stop moving along its [member patrolPath].
+var stopFollowingPath: bool = false
 ## The position that this [NPC] should look at.  Set to Vector3.ZERO if you don't want them to look at anything.  Cannot set to null due to [url]https://github.com/godotengine/godot-proposals/issues/162[/url]
 var lookAtPosition:Vector3 = Vector3.ZERO
-# Used to figure out the next position this NPC should go to along the patrolPath.
-var _pathFollower: PathFollow3D
-# Technically this can also modify the patrolSpeed, but it's only used for flipping the direction this NPC follows a path.
-var _pathFollowDirection: float = 1.0
-var _stopFollowingPath: bool = false
 
+# ------------------------------------------------
+# normal variables only referenced in script
+# ------------------------------------------------
+## [b]Internal-use only.[/b]  Used to figure out the next position this [NPC] should go to along the [member patrolPath].
+var _pathFollower: PathFollow3D
+## [b]Internal-use only.[/b]  The direction along the [member patrolPath] this [NPC] should go.  1.0 means it goes "forward," -1.0 means it goes "backward."  Can also modify the [member patrolSpeed].
+var _pathFollowDirection: float = 1.0
+
+# ------------------------------------------------
+# functions like _ready, _process, and _physics_process
+# ------------------------------------------------
 func _ready() -> void:
 	add_to_group("NPCs")
 
@@ -51,25 +79,33 @@ func _ready() -> void:
 		return
 	
 	if patrolEnabled:
-		_setup_path_follow()
+		_setupPathFollow()
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 	
-	if patrolEnabled and not _stopFollowingPath and patrolPath:
+	if patrolEnabled and not stopFollowingPath and patrolPath:
 		_moveOnPath(delta)
 	
 	if lookAtPosition != Vector3.ZERO:
 		_turnToLookAtPosition(delta)
 
-func _setup_path_follow() -> void:
+# ------------------------------------------------
+# functions referenced outside of this script
+# ------------------------------------------------
+
+# ------------------------------------------------
+# functions only referenced inside this script
+# ------------------------------------------------
+## [b]Internal-use only.[/b]  Sets up stuff related to patrolling.
+func _setupPathFollow() -> void:
 	if patrolPath == null:
 		return
 	
 	# Create PathFollow3D child node
 	_pathFollower = PathFollow3D.new()
-	_pathFollower.loop = (pathFollowMode == PATH_FOLLOW_METHOD.LOOP)
+	_pathFollower.loop = (pathFollowMode == PathFollowMethod.LOOP)
 	patrolPath.add_child(_pathFollower)
 	
 	# Start at closest point
@@ -79,23 +115,24 @@ func _setup_path_follow() -> void:
 	else:
 		_pathFollower.progress = 0.0
 
+## [b]Internal-use only.[/b]  Moves this [NPC] along the [member patrolPath].
 func _moveOnPath(delta:float) -> void:
 	var curve_len := patrolPath.curve.get_baked_length()
 	_pathFollower.progress += patrolSpeed * delta * _pathFollowDirection
 	
 	# Behavior when ping pong (going back and forth on path)
-	if (pathFollowMode == PATH_FOLLOW_METHOD.PING_PONG):
+	if (pathFollowMode == PathFollowMethod.PING_PONG):
 		if _pathFollower.progress >= curve_len:
 			_pathFollower.progress = curve_len
 			_pathFollowDirection = -1.0
-			_maybe_wait()
+			_waitForAMoment()
 		elif _pathFollower.progress <= 0.0:
 			_pathFollower.progress = 0.0
 			_pathFollowDirection = 1.0
-			_maybe_wait()
+			_waitForAMoment()
 	
 	# Behavior when on a closed curve w/ looping enabled
-	elif (pathFollowMode == PATH_FOLLOW_METHOD.LOOP):
+	elif (pathFollowMode == PathFollowMethod.LOOP):
 		_pathFollower.progress = fposmod(_pathFollower.progress, curve_len)
 	
 	# Undefined behavior - follows path once to completion
@@ -111,7 +148,7 @@ func _moveOnPath(delta:float) -> void:
 	# Move NPC on path
 	global_position = _pathFollower.global_position
 
-# Function to face whoever interacted with this [InteractableNPC]. Useful for moving NPCs.
+## [b]Internal-use only.[/b]  Turns this [NPC] to face the [member lookAtPosition].
 func _turnToLookAtPosition(delta: float) -> void:
 	var direction := lookAtPosition - global_position
 	direction.y = 0.0
@@ -120,20 +157,24 @@ func _turnToLookAtPosition(delta: float) -> void:
 	var target_dir := atan2(-direction.x, -direction.z)
 	
 	rotation.y = lerp_angle(rotation.y, target_dir, 5 * delta)
-	#rotation.y = target_dir
-	#model.rotation.y = lerp_angle(model.rotation.y, target_dir, 5 * delta)
 
-# Wait at end of path before going back, if wait time is declared
-func _maybe_wait() -> void:
+## [b]Internal-use only.[/b]  If this [NPC] is moving along its [member patrolPath],
+## this will make them stop for [member waitAtEndDuration] and then resume moving.
+func _waitForAMoment() -> void:
 	if waitAtEndDuration <= 0.0:
 		return
 	
-	_stopFollowingPath = true
+	stopFollowingPath = true
 	await get_tree().create_timer(waitAtEndDuration).timeout
-	_stopFollowingPath = false
+	stopFollowingPath = false
 
+# ------------------------------------------------
+# functions that run when a signal is emitted
+# ------------------------------------------------
 
-
+# ------------------------------------------------
+# editor dev-ing functions like "_get_configuration_warnings()"
+# ------------------------------------------------
 # Dev-ing stuff
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings:Array[String] = []
@@ -153,5 +194,5 @@ func _validate_property(property: Dictionary) -> void:
 		and not patrolEnabled:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
 	
-	if property.name in ["waitAtEndDuration"] and pathFollowMode != PATH_FOLLOW_METHOD.PING_PONG:
+	if property.name in ["waitAtEndDuration"] and pathFollowMode != PathFollowMethod.PING_PONG:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
