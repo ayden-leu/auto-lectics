25d3ddd6103375cf2321c0c196099ed474b9f9de
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-04-09 23:53:39 -0700

    a bunch of small fixes

--- fade_to_black_hitbox.gd
+++ fade_to_black_hitbox.gd
@@ -3,7 +3,11 @@ extends Node3D
 #@export var shouldIReloadTheScene = false
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
+	print("fadetoblackStarted")
 	$CanvasLayer/FadeRect/AnimationPlayer.play("fade_to_black")
+	#$CanvasLayer/Label2.visible
+	await get_tree().create_timer(1.0).timeout
+
 	$CanvasLayer/Label2/AnimationPlayer.play("text_fade_in")
 	$Timer.start()
 
