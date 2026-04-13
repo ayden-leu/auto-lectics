c3fc04337e396860c9a37bb37e70539360f97d80
ayden-leu <ajleu@ucdavis.edu>
2026-04-13 04:40:09 -0700

    passthrough (test_audio_loading)

--- helpers/audio_loader.gd
+++ helpers/audio_loader.gd
@@ -3,18 +3,24 @@ class_name AudioLoader
 ## Helper script to load audio files from project file structure.
 
 ## Loads audio files related to a given ID and loads theem into a given [AudioStreamRandomizer].
-static func loadSfxFromId(id:String, audioStream:AudioStreamRandomizer) -> void:
+static func loadSfxFromId(id:String, audioStream:AudioStreamRandomizer) -> int:
+	if id == "":
+		printerr("AudioLoader: No ID provided.")
+		return -1
+	
 	if id == "none":
-		return
+		return 0
 		
 	var tempDirAccess:DirAccess = DirAccess.open(Globals.STORAGE_PATH.SFX)
 	if not tempDirAccess.dir_exists(id):
 		printerr("AudioLoader: Could not find the SFX ID folder: ", id)
+		return -2
 	
 	var soundIDPath:String = Globals.STORAGE_PATH.SFX + id
 	var audioFileNames:PackedStringArray = ResourceLoader.list_directory(soundIDPath)
 	if audioFileNames.is_empty():
 		printerr("AudioLoader: Could not find any SFX in SFX ID folder: ", id)
+		return -3
 	
 	for filename in audioFileNames:
 		if not filename.ends_with(".wav"):
@@ -22,6 +28,8 @@ static func loadSfxFromId(id:String, audioStream:AudioStreamRandomizer) -> void:
 		
 		var audioFile:AudioStreamWAV = load(soundIDPath + "/" + filename)
 		audioStream.add_stream(-1, audioFile)
+	
+	return 0
 
 static func clearAudioRandomizer(audioStream:AudioStreamRandomizer) -> void:
 	for i in range(audioStream.streams_count):
