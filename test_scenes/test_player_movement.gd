3c879b8b7f2acc2073d7e7022dbcce26f259b227
ayden-leu <ajleu@ucdavis.edu>
2026-04-13 05:14:33 -0700

    forgot a couple things

--- test_scenes/test_player_movement.gd
+++ test_scenes/test_player_movement.gd
@@ -32,14 +32,6 @@ extends Node3D
 # ------------------------------------------------
 # functions like _ready, _process, and _physics_process
 # ------------------------------------------------
-#func _ready() -> void:
-	# super()  # needed if inheriting a custom class with its own _ready().  Will run the inherited class' _ready() function.
-
-#func _process(delta: float) -> void:
-	#super(delta)  # needed if inheriting a custom class with its own _process().  Will run the inherited class' _process() function.
-
-#func _physics_process(delta: float) -> void:
-	#super(delta)  # needed if inheriting a custom class with its own _physics_process().  Will run the inherited class' _physics_process() function.
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug_1"):
