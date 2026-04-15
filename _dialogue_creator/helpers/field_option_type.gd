6567023ce7dc3d99f7f15c4f8e5fbe9a43f7311b
ayden-leu <ajleu@ucdavis.edu>
2026-04-14 18:06:37 -0700

    passthrough* (dialogue creator helper stuff)
    *not complete

--- _dialogue_creator/helpers/field_option_type.gd
+++ _dialogue_creator/helpers/field_option_type.gd
@@ -1,13 +1,38 @@
 extends DC_FieldOption
 class_name DC_TypeFieldOption
+## TODO
 
+# ------------------------------------------------
+# signals
+# ------------------------------------------------
+
+# ------------------------------------------------
+# enums
+# ------------------------------------------------
+## The two types of types a [DC_BaseNode] can be.
 enum VALID_TYPES {
-	DIALOGUE,
-	OPTION
+	DIALOGUE,  ## Refer to [member DialogueDefaults.DIALOGUE_TYPES].
+	OPTION     ## Refer to [member DialogueDefaults.OPTION_TYPES].
 }
 
+# ------------------------------------------------
+# constants
+# ------------------------------------------------
+
+# ------------------------------------------------
+# export variables
+# ------------------------------------------------
+## The type of this [DC_BaseNode].
 @export var type:VALID_TYPES
 
+# ------------------------------------------------
+# onready variables
+# ------------------------------------------------
+
+# ------------------------------------------------
+# normal variables referenced outside of script
+# ------------------------------------------------
+## The chosen type.
 var option:String:
 	set(newOption):
 		chooser.selected = valueToOptionIndex[newOption]
@@ -19,6 +44,13 @@ var option:String:
 		printerr("field_option_type: Unhandled option type: ", type)
 		return "error"
 
+# ------------------------------------------------
+# normal variables only referenced in script
+# ------------------------------------------------
+
+# ------------------------------------------------
+# functions like _ready, _process, and _physics_process
+# ------------------------------------------------
 func _ready() -> void:
 	if type == VALID_TYPES.DIALOGUE:
 		fillValueToOptionIndex(DialogueDefaults.DIALOGUE_TYPES)
@@ -26,3 +58,19 @@ func _ready() -> void:
 		fillValueToOptionIndex(DialogueDefaults.OPTION_TYPES)
 	
 	option = DialogueDefaults.DEFAULT_DIALOGUE.type
+
+# ------------------------------------------------
+# functions referenced outside of this script
+# ------------------------------------------------
+
+# ------------------------------------------------
+# functions only referenced inside this script
+# ------------------------------------------------
+
+# ------------------------------------------------
+# functions that run when a signal is emitted
+# ------------------------------------------------
+
+# ------------------------------------------------
+# editor dev-ing functions like "_get_configuration_warnings()"
+# ------------------------------------------------
