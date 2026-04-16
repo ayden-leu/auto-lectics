b1757ba9675d2994edb7ac4ae24646aca1e3c8ba
ayden-leu <ajleu@ucdavis.edu>
2026-04-16 14:33:17 -0700

    moved files back

---
+++ entities/npcs/preacher.gd
@@ -0,0 +1,25 @@
+@tool
+extends InteractableNPC
+
+var time : float
+var base_position : Vector3
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	base_position = rotation
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	super(delta)
+	
+	time += delta;
+	rotation = Vector3(0, base_position.y +get_sine(), 0)
+	#position = Vector3(base_position.x, base_position.y + get_sine(), base_position.z)
+	#print(get_sine())
+	pass
+
+func get_sine():
+	return sin(time * 3) * 0.2
---
+++ world/gate.gd
@@ -0,0 +1,16 @@
+extends Node3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+			
+
+func open_gate():
+	
+		rotation.x = -90.0		
+		
+		#position += Vector3(0,-12,00) 
+		pass
+			 
