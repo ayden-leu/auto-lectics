707f63b65c1aadcdcfba6a8ec2035133e8306fe1
ayden-leu <ajleu@ucdavis.edu>
2026-04-24 12:25:43 -0700

    updated `open_gate` stuff
    now uses new command implementation

--- entities/npcs/looker.gd
+++ entities/npcs/looker.gd
@@ -7,7 +7,6 @@ var base_position : Vector3
 func _ready() -> void:
 	super()
 	base_position = position
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,3 +22,26 @@ func _process(delta: float) -> void:
 
 func get_sine():
 	return sin(time * 2) * 0.05
+
+# ----------------------------
+
+# NOTE 1
+# since InteractableNPCs already "subscribe" themselves to the DialogueConsole
+# when you interact with them, we don't have to add extra code to
+# "subscribe" to it, nor add an extra check to make sure this InteractableNPC
+# isTalking.
+#
+# NOTE 2
+# miniboss and the lookerNPC share the same script.  this means that entering
+# the `open_gate` command while talking to the lookerNPC will run the function.
+# However, it won't open the gate due to the check I added to see if
+# the $gateNode exists beforehand.
+# 
+# NOTE 3
+# if you want to make it so the `open_gate` command opens the gate regardless
+# of who opens the console, comment the below code out, and refer to
+# the script attached to the root node of the `spring_week_3_playtest.tscn` scene.
+func _on_console_command_entered(command:String) -> void:
+	if command == "open_gate":
+		if $gateNode:
+			$gateNode.open_gate()
