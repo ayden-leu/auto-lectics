59aecae25a12bb6540c1d01de1e751990ce91039
ayden-leu <ajleu@ucdavis.edu>
2026-05-29 16:03:18 -0700

    (docu) Options Menu

--- _framework/ui/menus/pause/options_menu.gd
+++ _framework/ui/menus/pause/options_menu.gd
@@ -1,5 +1,20 @@
 @tool
 extends Menu
+## A menu that lets players customize game settings.
+##
+## Comes with three buttons:[br]
+## - To open the Keybinds menu.[br]
+## - To modify the screen size.[br]
+## - To close this menu.[br]
+## [br][br]
+## The [member _applySettings] button is initially disabled and becomes enabled
+## when any of the settings are modified.
+## [br][br]
+## [b]Configurable Settings[/b][br]
+## There is currently only one setting:  Window Mode.
+## Window Mode can be set to to the presets defined in [member _windowPresets].
+## [br][br]
+## Also plays a sound whenever a button is clicked via its [member _sfxEventHandler].
 
 # ------------------------------------------------
 # signals
@@ -20,11 +35,15 @@ extends Menu
 # ------------------------------------------------
 # onready variables
 # ------------------------------------------------
-## [b]Internal-use only.[/b]  Holds thee window size modes that can be set.
+## [b]Internal-use only.[/b]
+## Holds the window size modes that can be chosen and set.
 @onready var _windowModeOptions:OptionButton = %WindowModeOptions
 ## [b]Internal-use only.[/b]
 ## The [SfxEventHandler] for this menu.
 @onready var _sfxEventHandler:SfxEventHandler = %SfxEventHandler
+## [b]Internal-use only.[/b]
+## The button that applies all of the configured settings.
+@onready var _applySettings: Button = %ApplySettings
 
 # ------------------------------------------------
 # normal variables referenced outside of script
@@ -34,9 +53,12 @@ extends Menu
 # normal variables only referenced in script
 # [b]Internal-use only.[/b]
 # ------------------------------------------------
-
+## [b]Internal-use only.[/b]
+## The modes the game window can be in.
 var _windowPresets = [
 	{"name": "Windowed",             "mode": DisplayServer.WINDOW_MODE_WINDOWED,   "flag": 0},
+	{"name": "Borderless",           "mode": DisplayServer.WINDOW_MODE_WINDOWED,   "flag": DisplayServer.WINDOW_FLAG_BORDERLESS},
+	{"name": "Maximized",            "mode": DisplayServer.WINDOW_MODE_MAXIMIZED,  "flag": 0},
 	{"name": "Fullscreen",           "mode": DisplayServer.WINDOW_MODE_FULLSCREEN, "flag": 0},
 	{"name": "Exclusive Fullscreen", "mode": DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN, "flag": 0}
 ]
@@ -51,6 +73,7 @@ func _ready() -> void:
 	menuID = "options"
 	super()  # runs the inherited class' _ready() function.
 
+	# loads the _windowPresets into _windowModeOptions
 	var i:int = 0
 	for preset in _windowPresets:
 		_windowModeOptions.add_item(preset.name)
@@ -66,8 +89,10 @@ func _ready() -> void:
 # functions only referenced inside this script
 # [b]Internal-use only.[/b]
 # ------------------------------------------------
-## [b]Internal-use only.[/b]  Updates the window size mode.
+## [b]Internal-use only.[/b]
+## Updates the window size mode.
 func _updateWindowMode(newMode:String) -> void:
+	_resetFlags()
 	for preset in _windowPresets:
 		if preset.name != newMode:
 			continue
@@ -75,29 +100,41 @@ func _updateWindowMode(newMode:String) -> void:
 		DisplayServer.window_set_mode(preset.mode)
 		if preset.flag != 0:
 			DisplayServer.window_set_flag(preset.flag, true)
+		return
+
+## [b]Internal-use only.[/b]
+## Resets all possible window flags that could have been set via [member _windowPresets].
+func _resetFlags() -> void:
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
 
 # ------------------------------------------------
 # functions that run when a signal is emitted
 # ------------------------------------------------
-
-#func _on_open_submenu_pressed() -> void:
-#_TS_open.emit(subMenu)
-
-# ------------------------------------------------
-# editor dev-ing functions like "_get_configuration_warnings()"
-# ------------------------------------------------
-## [b]Internal-use only.[/b]  Handles logic for when the keybinds button is pressed.
+## [b]Internal-use only.[/b]
+## Handles logic for when the keybinds button is pressed.
 func _on_keybinds_pressed() -> void:
 	_sfxEventHandler.play("buttonPressed")
 	FR_MenuManager.openMenu("keybinds")
 
-## [b]Internal-use only.[/b]  Handles logic for when the back button is pressed.
+## [b]Internal-use only.[/b]
+## Handles logic for when the back button is pressed.
 func _on_back_pressed() -> void:
 	_sfxEventHandler.play("buttonPressed")
 	close()
 
-## [b]Internal-use only.[/b]  Handles logic for when the apply settings button is pressed.
+## [b]Internal-use only.[/b]
+## Handles logic for when the [member _applySettings] button is pressed.
 func _on_apply_settings_pressed() -> void:
 	_sfxEventHandler.play("buttonPressed")
 	var selectedMode:String = _windowModeOptions.get_item_text(_windowModeOptions.selected)
 	_updateWindowMode(selectedMode)
+	_applySettings.disabled = true
+
+## [b]Internal-use only.[/b]
+## Handles logic for when an option from the [member _windowModeOptions] button is picked.
+func _on_window_mode_options_item_selected(_index:int) -> void:
+	_applySettings.disabled = false
+
+# ------------------------------------------------
+# editor dev-ing functions like "_get_configuration_warnings()"
+# ------------------------------------------------
