725e2e9e1dbfb0cf1272e84844a11706a1207be7
ayden-leu <ajleu@ucdavis.edu>
2026-05-30 12:41:58 -0700

    (docu) InputHandler passthrough

--- _framework/helpers/input_handler.gd
+++ _framework/helpers/input_handler.gd
@@ -3,12 +3,19 @@ extends Node
 class_name InputHandler
 ## Handles all inputs a player can possibly make.
 ##
-## When a player executes and input, a signal for that input will be emitted.
-## To use, attach this to your node and connect the corresponding signals.
+## Well, almost all of them.  [code]open_blueprint[/code] gets handled by [WindowManager]
+## since the input shouldn't be handled while the user is typing.
 ## [br][br]
+## When a player executes and input, a signal for that input will be emitted.
+## [br][br][br]
+## [b]Using[/b][br]
+## To use, attach this to your node and connect the corresponding signals
+## to functions in your node's script.
+## [br][br][br]
+## [b]Things to keep in mind:[/b][br]
 ## The input direction passed with the [signal update_input_direction] signal
-## is normalized, meaning diagonal inputs will be equal to the square root of 2
-## instead of 1.
+## is normalized, meaning diagonal inputs will be equal to [code]the square root of 2[/code]
+## instead of [code]1[/code].
 ## [br][br]
 ## The move distance passed with the [signal mouse_moved] signal is multiplied
 ## by [member MOUSE_SENSITIVITY] before being sent along the way.
@@ -17,18 +24,18 @@ class_name InputHandler
 # signals
 # ------------------------------------------------
 ## Emitted when the mouse moves.
+## [br]
 ## [code]distanceMoved[/code] is multiplied by [member MOUSE_SENSITIVITY] before
 ## being sent with the signal.
-
 signal mouse_moved(distanceMoved:Vector2)
 ## Emitted when the [code]interact[/code] action is pressed.
 signal interact_button_pressed()
 ## Emitted when the [code]jump[/code] action is pressed.
 signal jump_pressed()
 ## Emitted constantly to update the player's current input direction.
+## [br]
 ## [code]newDirection[/code] is normalized, meaning diagonal inputs will be
 ## equal to the square root of 2 instead of 1.
-
 signal update_input_direction(newDirection:Vector2)
 ## Emitted when the [code]respawn[/code] action is pressed.
 signal respawn()
@@ -43,8 +50,8 @@ signal grapple_pressed()
 # constants
 # ------------------------------------------------
 ## Multiplier applied to raw mouse motion before emitting [signal mouse_moved].
-## [br][br]
-## Larger values make emitted mouse movement stronger.  Camera scripts may also apply their own sensitivity after receiving the signal.
+## [br]
+## Larger values make emitted mouse movement stronger.
 const MOUSE_SENSITIVITY:float = 0.15
 
 # ------------------------------------------------
@@ -60,52 +67,38 @@ const MOUSE_SENSITIVITY:float = 0.15
 # ------------------------------------------------
 ## Whether movement input should be processed by this InputHandler instance.
 ## [br][br]
-## When false, this instance emits [constant Vector2.ZERO] through [signal update_input_direction] even if the player is pressing movement keys.  This does not affect other [InputHandler] nodes.
+## When [code]false[/code], this instance emits [constant Vector2.ZERO] through [signal update_input_direction]
+## even if the player is pressing movement keys.
+## Does not affect all InputHandlers globally.
+## (i.e one InputHandler having this [code]false[/code] only affects that one InputHandler).
 var movementInputEnabled:bool = true
 
 # ------------------------------------------------
 # normal variables only referenced in script
 # ------------------------------------------------
 ## [b]Internal-use Only.[/b]
-## Whether movement input should be processed by all [InputHandler] instances.
+## Whether movement input should be processed by all InputHandler instances.
 static var _movementInputEnabledGlobal:bool = true
-
-## Whether interaction inputs should be processed.  Affects all [InputHandler]s.
+## [b]Internal-use Only.[/b]
+## Whether interaction inputs should be processed.  Affects all InputHandlers.
 static var _interactionEnabledGlobal:bool = true
-## Whether the jump input should be processed.  Affects all [InputHandler]s.
+## [b]Internal-use Only.[/b]
+## Whether the jump input should be processed.  Affects all InputHandlers.
 static var _jumpEnabledGlobal:bool = true
-## Whether the respawn input should be processed.  Affects all [InputHandler]s.
+## [b]Internal-use Only.[/b]
+## Whether the respawn input should be processed.  Affects all InputHandlers.
 static var _respawnEnabledGlobal:bool = true
-
 
 # ------------------------------------------------
 # functions like _ready, _process, and _physics_process
 # ------------------------------------------------
-
-## [b]Internal-use Only.[/b]
-## Captures the mouse when this handler enters the scene tree.
-## [br][br]
-## This lets first-person camera controls receive relative mouse movement during gameplay.
-func _ready() -> void:
-	#if Engine.is_editor_hint():
-		#return
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-## [b]Internal-use Only.[/b]
-## Emits [signal update_input_direction] every physics frame.
-## [br][br]
-## If either [member movementInputEnabled] or the global movement-input flag is false, this emits [constant Vector2.ZERO].
-
 func _physics_process(_delta: float) -> void:
+	# handle inputted directions
 	var input_dir:Vector2 = Vector2.ZERO
 	if _movementInputEnabledGlobal and movementInputEnabled:
 		input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	update_input_direction.emit(input_dir)
 
-## [b]Internal-use Only.[/b]
-## Handles action presses and mouse motion.
-## [br][br]
-## Emits the matching signals for [code]interact[/code], [code]jump[/code], [code]respawn[/code], and mouse movement.  If [code]close_game[/code] is pressed, the scene tree quits immediately.
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and _interactionEnabledGlobal:
 		interact_button_pressed.emit()
@@ -135,6 +128,7 @@ func _input(event: InputEvent) -> void:
 # ------------------------------------------------
 # functions referenced outside of this script
 # ------------------------------------------------
+## Lets movement inputs be processed for all InputHandlers.
 static func enableMovementInputGlobal() -> void:
 	_movementInputEnabledGlobal = true
 
@@ -144,27 +138,27 @@ static func enableMovementInputGlobal() -> void:
 static func disableMovementInputGlobal() -> void:
 	_movementInputEnabledGlobal = false
 
-## Lets interaction inputs be processed.
+## Lets interaction inputs be processed for all InputHandlers.
 static func enableInteractionInputGlobal() -> void:
 	_interactionEnabledGlobal = true
 
-## Prevents interaction inputs from being processed.
+## Prevents interaction inputs from being processed for all InputHandlers.
 static func disableInteractionInputGlobal() -> void:
 	_interactionEnabledGlobal = false
 
-## Lets jump inputs be processed.
+## Lets jump inputs be processed for all InputHandlers.
 static func enableJumpInputGlobal() -> void:
 	_jumpEnabledGlobal = true
 
-## Prevents jump inputs from being processed.
+## Prevents jump inputs from being processed for all InputHandlers.
 static func disableJumpInputGlobal() -> void:
 	_jumpEnabledGlobal = false
 
-## Lets the respawn input be processed.
+## Lets the respawn input be processed for all InputHandlers.
 static func enableRespawnInputGlobal() -> void:
 	_respawnEnabledGlobal = true
 
-## Prevents the respawn input from being processed.
+## Prevents the respawn input from being processed for all InputHandlerss.
 static func disableRespawnInputGlobal() -> void:
 	_respawnEnabledGlobal = false
 
